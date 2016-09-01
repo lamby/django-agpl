3a44dbeec871aa057c4d5b42c9089a8d2b649063
Chris Lamb <chris@chris-lamb.co.uk>
2016-09-01 22:36:25 +0100

    Drop patterns import for Django 1.0 compatibility.

--- django_agpl/urls.py
+++ django_agpl/urls.py
@@ -16,12 +16,12 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from . import views
 
 
-urlpatterns = patterns('django_agpl.views',
+urlpatterns = (
     url(r'^tar$', views.tar,
         name='download-tar'),
     url(r'^zip$', views.zip,
