75a8dd80c69c83a6588df882275d65ecbfa0b26a
Chris Lamb <chris@chris-lamb.co.uk>
2016-09-01 22:16:41 +0100

    Update copyright years.

--- django_agpl/utils.py
+++ django_agpl/utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # django-agpl -- tools to aid releasing Django projects under the AGPL
-# Copyright (C) 2008, 2009 Chris Lamb <chris@chris-lamb.co.uk>
+# Copyright (C) 2008, 2009, 2016 Chris Lamb <chris@chris-lamb.co.uk>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
